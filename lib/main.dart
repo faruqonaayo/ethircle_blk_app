e72add7febaab2fe6a4fafad42a815f092d139a3
Faruq Ayomide <onaayofaruq24@gmail.com>
2025-09-08 12:31:22 -0500

    Implement app layout with bottom navigation and theme support

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,44 @@
 import 'package:flutter/material.dart';
 
+import 'package:ethircle_blk_app/screens/app_layout.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 void main() {
   runApp(const BLKApp());
 }
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromRGBO(34, 40, 49, 1),
+  brightness: Brightness.light,
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromRGBO(34, 40, 49, 1),
+  brightness: Brightness.dark,
+);
+
 class BLKApp extends StatelessWidget {
   const BLKApp({super.key});
+
+  ThemeData getAppThemeData(ColorScheme colorScheme) {
+    return ThemeData().copyWith(
+      colorScheme: colorScheme,
+      scaffoldBackgroundColor: colorScheme.surface,
+      textTheme: GoogleFonts.latoTextTheme().apply(
+        bodyColor: colorScheme.onSurface,
+        displayColor: colorScheme.onSurface,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Ethircle BLK Application',
-      home: Scaffold(body: Text("Welcome to BLK App!")),
+      themeMode: ThemeMode.light,
+      theme: getAppThemeData(kColorScheme),
+      darkTheme: getAppThemeData(kDarkColorScheme),
+      home: AppLayout(),
     );
   }
 }
---
+++ lib/screens/app_layout.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class AppLayout extends StatefulWidget {
+  const AppLayout({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _AppLayoutState();
+  }
+}
+
+class _AppLayoutState extends State<AppLayout> {
+  var _currentPage = 0;
+
+  final _pages = const [
+    Text("Home"),
+    Text("Categories"),
+    Text("Add Data"),
+    Text("Favorites"),
+    Text("Profile"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    var colorScheme = Theme.of(context).colorScheme;
+    return Scaffold(
+      appBar: AppBar(),
+      body: IndexedStack(index: _currentPage, children: _pages),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: colorScheme.surfaceContainerLow,
+        unselectedItemColor: colorScheme.secondary,
+        selectedItemColor: colorScheme.primary,
+        currentIndex: _currentPage,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category_outlined),
+            label: "Categories",
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              width: 32,
+              height: 32,
+              decoration: BoxDecoration(
+                color: colorScheme.primary,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Icon(Icons.add, color: colorScheme.onPrimary),
+            ),
+            label: "Add",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_outline),
+            label: "Favorites",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: "Profile",
+          ),
+        ],
+        onTap: (value) {
+          // Tapping on the add icon in the navigation bar acts differently by displaying a modal bottom sheet with options
+          if (value == 2) {
+            showModalBottomSheet(
+              context: context,
+              builder: (ctx) => Container(height: 160),
+            );
+            return;
+          }
+          setState(() {
+            _currentPage = value;
+          });
+        },
+      ),
+    );
+  }
+}
