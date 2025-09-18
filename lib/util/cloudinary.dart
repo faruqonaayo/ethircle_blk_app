29d1a032e9addb2960ed62bdac252e98a838bf6c
Faruq Ayomide <onaayofaruq24@gmail.com>
2025-09-18 16:03:39 -0500

    feat: Add Cloudinary integration for file uploads and update dependencies

---
+++ lib/util/cloudinary.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:cloudinary_url_gen/cloudinary.dart';
+import 'package:http/http.dart' as http;
+
+final cloudinaryObj = Cloudinary.fromCloudName(cloudName: "du7eu4jhl");
+
+void uploadToCloudinary(File file) async {
+  final url = Uri.parse("https://api.cloudinary.com/v1_1/du7eu4jhl/upload");
+
+  final request = http.MultipartRequest("POST", url);
+
+  request.fields["upload_preset"] = "maablk";
+
+  final fileToUpload = await http.MultipartFile.fromPath("file", file.path);
+  request.files.add(fileToUpload);
+
+  final response = await request.send();
+
+  if (response.statusCode == 200) {
+    // convert the response to readable map
+    final responseStr = await response.stream.bytesToString();
+
+    final result = jsonDecode(responseStr);
+    print(result["url"]);
+    
+  }
+}
