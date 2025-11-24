5d4a50a6af0ca10237d3402771ddf045ef912144
Faruq Ayomide <onaayofaruq24@gmail.com>
2025-11-23 18:34:02 -0600

    Add initial project setup with Flutter configuration and theme implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:ethircle_blk_app/theme.dart';
 
 void main() {
   runApp(const BLKApp());
 }
+
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromRGBO(60, 73, 63, 1),
+  brightness: Brightness.light,
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromRGBO(60, 73, 63, 1),
+  brightness: Brightness.dark,
+);
 
 class BLKApp extends StatelessWidget {
   const BLKApp({super.key});
@@ -11,7 +24,28 @@ class BLKApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Ethircle BLK App",
-      home: Scaffold(body: Center(child: Text("Hello BLK App"))),
+      themeMode: ThemeMode.light,
+      theme: getThemeData(kColorScheme),
+      darkTheme: getThemeData(kDarkColorScheme),
+      home: Scaffold(
+        body: Center(child: Text("Hello BLK App", style: title1)),
+      ),
+    );
+  }
+
+  // This function uses the provided color scheme to return the theme configuration of the application
+  ThemeData getThemeData(ColorScheme colorScheme) {
+    // Body text default style
+    final bodyDefaultStyle = GoogleFonts.openSans(color: colorScheme.onSurface);
+
+    return ThemeData().copyWith(
+      colorScheme: colorScheme,
+      scaffoldBackgroundColor: colorScheme.surface,
+      textTheme: TextTheme().copyWith(
+        bodyLarge: bodyDefaultStyle.copyWith(fontSize: 18),
+        bodyMedium: bodyDefaultStyle.copyWith(fontSize: 16),
+        bodySmall: bodyDefaultStyle.copyWith(fontSize: 14),
+      ),
     );
   }
 }
---
+++ lib/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final title1 = GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.bold);
+final title2 = GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.bold);
+final title3 = GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.bold);
